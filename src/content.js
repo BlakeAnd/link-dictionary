0774eaba8cf84c24cb1d248d15b7bd316c9ad770
Blake Mitchell <mblakeandrew@gmail.com>
2020-05-06 20:47:47 -0700

    attempting to detect key pressed

--- src/content.js
+++ src/content.js
@@ -1,3 +1,9 @@
 document.onkeydown = function() {
   console.log("test");
+  if (event.keyCode === 113){
+    console.log("queue")
+  }
 };
+
+// chrome.storage.local.set({ isPaused: false })
+// chrome.storage.local.set({'user_name': json.user_name})
