c0e335c3488be14b1de57fae5877402336850c05
Blake Mitchell <mblakeandrew@gmail.com>
2020-05-06 22:39:50 -0700

    wrapped document in event listener to wait until content is loaded

--- src/content.js
+++ src/content.js
@@ -1,29 +1,36 @@
-let a_class = document.getElementsByClassName("rm-title-display");
-console.log("class log", a_class);
+window.addEventListener('load', function () {
+    console.log("It's loaded!")
+  
+    let a_class = document.getElementsByClassName("rm-title-display");
+    console.log("class log", a_class);
+    
+    let nodes = a_class;
+    console.log("span log", nodes);
+    
+    document.onkeypress = function() {
+      console.log("test");
+      // console.log(event.keyCode);
+    
+    
+      // if(document.getElementsByClassName("shoutbox").length > 0){
+      //   alert('Chat is available.')
+      // }
+      if (event.keyCode == 97){
+        console.log(event.keyCode, String.fromCharCode(event.keyCode)); 
+        // event.preventDefault();
+        console.log("the id", event.id);
+      }
+    };
+    
+    // document.addEventListener('DOMSubtreeModified', function(e) {
+    //     if(document.getElementsByClassName("shoutbox").length > 0){
+    //         alert('Chat is available.')
+    //     }
+    // });
+    
+    // chrome.storage.local.set({ isPaused: false })
+    // chrome.storage.local.set({'user_name': json.user_name})  
+  
+})
 
-let nodes = a_class.childNodes;
-console.log("span log", a_span);
 
-document.onkeypress = function() {
-  console.log("test");
-  // console.log(event.keyCode);
-
-
-  // if(document.getElementsByClassName("shoutbox").length > 0){
-  //   alert('Chat is available.')
-  // }
-  if (event.keyCode == 97){
-    console.log(event.keyCode, String.fromCharCode(event.keyCode)); 
-    // event.preventDefault();
-    console.log("the id", event.id);
-  }
-};
-
-// document.addEventListener('DOMSubtreeModified', function(e) {
-//     if(document.getElementsByClassName("shoutbox").length > 0){
-//         alert('Chat is available.')
-//     }
-// });
-
-// chrome.storage.local.set({ isPaused: false })
-// chrome.storage.local.set({'user_name': json.user_name})
