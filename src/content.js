058ba57206c0ca2958c07f37bff8c96072a9a1f7
Blake Mitchell <mblakeandrew@gmail.com>
2020-05-09 16:03:17 -0700

    working on storing words in link dictionary and checking against it

--- src/content.js
+++ src/content.js
@@ -6,11 +6,17 @@
     
     let nodes = a_class;
     console.log("span log", nodes);
+
     
     document.onkeypress = function() {
       console.log("test");
       console.log(event.keyCode, event.Charcode);
-    
+
+      let dictionary = {
+        "key concepts": "active",
+        "dreams": "active",
+        "to read": "active"
+      }
     
       // if(document.getElementsByClassName("shoutbox").length > 0){
       //   alert('Chat is available.')
@@ -18,20 +24,24 @@
       if (event.keyCode == 32){
         console.log(event.keyCode, String.fromCharCode(event.keyCode)); 
         // event.preventDefault();
-        console.log(document.activeElement.innerHTML);
+        //console.log(document.activeElement.innerHTML);
         let str = document.activeElement.innerHTML;
         // console.log(str[str.length-1]);
         let space_found = false;
+        let word_string = "";
         for(i = str.length-1; i > 0; i--){
+          word_string = word_string + str;
           console.log(str[i]);
           if(str[i] === " " && space_found === false){
             space_found = true;
+            // console.log("string to end:", str.slice(i+1));
             str = str.slice(0, i+1) + "[[" + str.slice(i+1) + "]]"
             break;
           } else if (i === 1){
             str = "[[" + str + "]]"
           }
         }
+        console.log("WORD", word_string);
         // str = str + "]]";
         // console.log(str)
         // // document.getElementById("block-input-PpsjPhmnnjdkWYiBg2nnDTjHHOA2-body-outline-05-08-2020-k5YKEc8J1").innerHTML = "hey!";
