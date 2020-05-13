1695685b35aab06c28837325e13d729febb83644
Blake Mitchell <mblakeandrew@gmail.com>
2020-05-12 22:29:00 -0700

    setting up local storage, got dictionary display function and add mostly working

--- README.md
+++ README.md
@@ -16,3 +16,22 @@ add any word with [[]] around it to a dictionary(type thing) [working on / diffe
 search the dictionary of words for all future words (every keystroke? every space so it only fires at the end of the word?)
 
 if it matches, inject the [[]] around the word (not sure if possible, but i think so)
+
+
+.
+.
+.
+
+logic to check a string (multi word)
+
+upon hitting space
+loop backwards through chars in string from end to cover longest stored string
+store location of every space character along the way
+at the end of the loop, check if last(leftmost) character is a space, as it should be if the string matches
+if yes, check if string matches
+if no to either, move right to next space and check if there is a matching string from there to the end, repeat til last space
+
+if a string match occurs, link it and stop checking
+
+WRONG, THIS WILL ONLY WORK WITH LINEAR TYPING. NEED TO CHECK WHOLE DOC BECAUSE WORDS CAN BE ADDED TO MIDDLE OR CHECK IF CHARACTER INSERT LOCATION CAN BE FOUND
+
--- src/content.js
+++ src/content.js
@@ -12,41 +12,126 @@
       console.log("test");
       console.log(event.keyCode, event.Charcode);
 
-      let dictionary = {
-        "key concepts": "active",
-        "dreams": "active",
-        "to read": "active"
-      }
+      // let dictionary = {
+      //   "key concepts": "active",
+      //   "dreams": "active",
+      //   "to read": "active"
+      // }
+
+      let dictionary = chrome.storage.local;
+      
+      dictionary.set({"dreams":"active"}, function() {
+        // console.log('Value is set to ' + value);
+      });
     
+      dictionary.get(['dreams'], function(result) {
+        return result;
+      });
+      console.log("local store", dictionary.get(['dream']));
+
+      let listen = -1;
       // if(document.getElementsByClassName("shoutbox").length > 0){
       //   alert('Chat is available.')
       // }
-      if (event.keyCode == 32){
-        console.log(event.keyCode, String.fromCharCode(event.keyCode)); 
-        // event.preventDefault();
-        //console.log(document.activeElement.innerHTML);
-        let str = document.activeElement.innerHTML;
-        // console.log(str[str.length-1]);
-        let space_found = false;
-        let word_string = "";
-        for(i = str.length-1; i > 0; i--){
-          word_string = word_string + str;
-          console.log(str[i]);
-          if(str[i] === " " && space_found === false){
-            space_found = true;
-            // console.log("string to end:", str.slice(i+1));
-            str = str.slice(0, i+1) + "[[" + str.slice(i+1) + "]]"
-            break;
-          } else if (i === 1){
-            str = "[[" + str + "]]"
-          }
-        }
-        console.log("WORD", word_string);
-        // str = str + "]]";
-        // console.log(str)
-        // // document.getElementById("block-input-PpsjPhmnnjdkWYiBg2nnDTjHHOA2-body-outline-05-08-2020-k5YKEc8J1").innerHTML = "hey!";
-        document.activeElement.value = str;
+      if (event.keyCode == listen){
+        listen = -1;
+        //other logic
       }
+      else if (event.keyCode == 32){
+          //space event, check previous char to make decision
+          //if previous char is +, loop back to previous space and link word (same function as multi word should work
+          // get_cursor_position();
+
+
+
+        // console.log(event.keyCode, String.fromCharCode(event.keyCode)); 
+        // // event.preventDefault();
+        // //console.log(document.activeElement.innerHTML);
+        // let str = document.activeElement.innerHTML;
+        // // console.log(str[str.length-1]);
+        // let space_found = false;
+        // let word_string = "";
+        // for(i = str.length-1; i > 0; i--){
+        //   word_string = word_string + str;
+        //   console.log(str[i]);
+        //   if(str[i] === " " && space_found === false){
+        //     space_found = true;
+        //     // console.log("string to end:", str.slice(i+1));
+        //     str = str.slice(0, i+1) + "[[" + str.slice(i+1) + "]]"
+        //     break;
+        //   } else if (i === 1){
+        //     str = "[[" + str + "]]"
+        //   }
+        // }
+        // console.log("WORD", word_string);
+        // // str = str + "]]";
+        // // console.log(str)
+        // // // document.getElementById("block-input-PpsjPhmnnjdkWYiBg2nnDTjHHOA2-body-outline-05-08-2020-k5YKEc8J1").innerHTML = "hey!";
+
+        // document.activeElement.value = str;
+      }
+      else if (event.keyCode == 43){
+        handle_plus(dictionary);
+      }
+      else if (event.keyCode == 45){
+        remove_word(dictionary);  
+      }
+      console.log("dict outside func", dictionary)
+    };
+
+    function show_dictionary(dictionary){
+      console.log("dict in show func", dictionary)
+      let dict_display = ""
+      for(let word in dictionary){
+        console.log(word)
+        dict_display = dict_display + word + ", ";
+      }
+      dict_display = dict_display.slice(0, dict_display.length-2);
+      document.activeElement.value = dict_display;
+    }
+
+    function handle_plus (dictionary) {
+      let str = document.activeElement.value;
+      let inner_str = str.slice(1);
+      console.log("in idct", dictionary[inner_str]);
+      console.log("len", inner_str.length);
+      // if (inner_str.length === 0 && str[0] === "+" && char_position === 1){
+      if(str === "+"){
+        event.preventDefault();
+      // console.log("len again", inner_str.length);
+        show_dictionary(dictionary);
+      } else if(str[0] === "+" && dictionary[inner_str] != "active"){
+        event.preventDefault(); 
+        add_word(dictionary, inner_str);
+      }
+    }
+
+    function add_word(dictionary, inner_str){
+      console.log("dict before", dictionary);
+      dictionary[inner_str] = "active";
+      console.log("dict after", dictionary);
+      document.activeElement.value = "";
+    }
+
+    function remove_word(dictionary){
+      let str = document.activeElement.value;
+      let inner_str = str.slice(1);
+      console.log("in idct", dictionary[inner_str]);
+      if(str[0] === "-" && dictionary[inner_str] === "active"){
+        dictionary[inner_str] = "inactive";
+      }
+      document.activeElement.value = "";
+    }
+
+    function get_cursor_position(){
+      let cursor_position = null;
+      if(document.activeElement.selectionStart === document.activeElement.selectionEnd){
+        cursor_position = document.activeElement.selectionStart;
+      }
+      // console.log("cursor", document.activeElement.value.length);
+      // console.log("cursor", document.activeElement.selectionStart);
+      // console.log("cursor", document.activeElement.selectionEnd);
+      return cursor_position;
     };
     
     // document.addEventListener('DOMSubtreeModified', function(e) {
